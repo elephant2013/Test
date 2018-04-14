178d32798b42083d52ceec09a18846b477a77e43
elephant2013 <elephant2013@qq.com>
2018-04-14 19:32:34 +0800

    第一次更新

---
+++ src/com/immoc/Test01.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.immoc;
+
+import java.util.Scanner;
+
+/**
+ * @author Focus_U
+ *
+ */
+public class Test01 {
+
+	/**
+	 * 
+	 */
+	public Test01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		input.next();
+		System.out.println("你好");
+	}
+
+}
---
+++ src/com/immoc/Test02.java
@@ -0,0 +1,16 @@
+package com.immoc;
+	
+	public class Test02 {
+		public void print() {
+			System.out.println("Hello world !");
+		}
+		public static void main(String[] args) {
+		Test02 test=new Test02();
+		test.print();
+	}
+		@Override
+		public String toString() {
+			return "Test02 []";
+		}
+		
+}
